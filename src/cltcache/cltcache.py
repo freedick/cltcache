0a34b6b6bf736b1155ed5a55ce7f8638c5665e25
Fredrik Nordin <freedick@users.noreply.github.com>
2022-06-22 17:00:26 +0200

    Add config file and remove temp files (#1)
    * add config file and remove temp files
    
    * add doc about config
    
    * reduce verbosity for preprocessing status
    
    * add hit/miss info in verbose mode
    
    * make cltcache print statements easily traceable
    
    * fix elif mistake
    
    * change formatting for print statements
    
    * fix bug and add additional verbose printouts
    
    Co-authored-by: Fredrik Nordin <fredrik.y.nordin@scania.com>

--- src/cltcache/cltcache.py
+++ src/cltcache/cltcache.py
@@ -11,6 +11,7 @@ import re
 import subprocess
 import sys
 import time
+import configparser
 
 
 def save_to_file_raw(data, filename):
@@ -39,16 +40,6 @@ def decompress_from_file(filename):
     return gzip.decompress(read_from_file_raw(filename))
 
 
-def sha256file(filename):
-    with open(filename, "rb") as f:
-        m = hashlib.sha256()
-        while True:
-            contents = f.read(65536)
-            if not contents:
-                return m.hexdigest()
-            m.update(contents)
-
-
 def sha256(string):
     m = hashlib.sha256()
     m.update(string.encode('utf-8'))
@@ -64,47 +55,49 @@ def run_command(command):
                           stderr=subprocess.PIPE, check=False)
 
 
-def run_get_stdout(command):
+def run_get_stdout(command, ignore_errors=False):
     """
     Run a command and forward its stdout and stderr if exit code is nonzero,
     otherwise return stdout.
     """
     result = run_command(command)
-    if result.returncode != 0:
-        print(result.stdout.decode("utf-8"),)
-        print(result.stderr.decode("utf-8"), file=sys.stderr,)
-        print("cltcache failed!", file=sys.stderr)
-        sys.exit(result.returncode)
+    if not ignore_errors and result.returncode != 0:
+        raise Exception(f"Bad exit code when running: {command}")
     return result.stdout.decode("utf-8")
 
 
-def get_preproc_hash(compile_args):
-    preproc_filename = sha256(" ".join(compile_args)) + "-cltcache.i"
+def remove_o_flag(compile_args):
     if "-o" in compile_args:
         oflag_index = compile_args.index('-o')
-        preproc_filename = compile_args[oflag_index + 1] + "-cltcache.i"
-        compile_args[oflag_index + 1] = preproc_filename
-    else:
-        compile_args = ["-o", preproc_filename] + compile_args
+        return compile_args[:oflag_index] + compile_args[oflag_index + 2:]
+    return compile_args
+
+
+def get_preproc_hash(compile_args, config):
+    compile_args = remove_o_flag(compile_args)
     preproc_flag = "-E"
-    keep_comments_flag = "-CC"
-    run_get_stdout(
-        ["g++"] + compile_args + [preproc_flag, keep_comments_flag])
-    preproc_hash = sha256file(preproc_filename)
-    os.remove(preproc_filename)
+    keep_comments_flag = config.get(
+        "preprocessor", "preserve_comments", fallback="-C")
+    preproc_source = run_get_stdout(
+        ["g++"] + compile_args + [preproc_flag, keep_comments_flag],
+        config.getboolean("preprocessor", "ignore_errors", fallback=False))
+    verbose = config.getboolean("behavior", "verbose", fallback=False)
+    if verbose:
+        print("cltcache length of preproccesed source:", len(preproc_source))
+    preproc_hash = sha256(preproc_source)
     return preproc_hash
 
 
-def compute_cache_key(clang_tidy_call):
+def compute_cache_key(clang_tidy_call, config):
     clang_tidy = clang_tidy_call[0]
     if "--" not in clang_tidy_call:
-        print("Missing '--' flag in compiler options", file=sys.stderr)
-        sys.exit(1)
+        raise Exception("Missing '--' flag in compiler options")
+
     forwardflag_index = clang_tidy_call.index("--")
     compile_args = clang_tidy_call[forwardflag_index + 1:]
     clang_tidy_args = clang_tidy_call[1:forwardflag_index]
 
-    preproc_hash = get_preproc_hash(compile_args)
+    preproc_hash = get_preproc_hash(compile_args, config)
 
     version_out = run_get_stdout([clang_tidy] + ["--version"])
     version = ",".join(re.findall(r'[0-9]+\.[0-9]+\.?[0-9]*', version_out))
@@ -118,39 +111,67 @@ def compute_cache_key(clang_tidy_call):
 
 
 def init_cltcache():
-    cltcache_path = pathlib.Path().home() / ".cltcache"
+    cltcache_path = os.environ.get(
+        "CLTCACHE_DIR", pathlib.Path().home() / ".cltcache")
     cltcache_path.mkdir(parents=True, exist_ok=True)
-    return cltcache_path
+    config = configparser.ConfigParser()
+    config.read(cltcache_path / "cltcache.cfg")
+    return cltcache_path, config
 
 
 def cache_clang_tidy(clang_tidy_call):
-    cltcache_path = init_cltcache()
-    cache_key = compute_cache_key(clang_tidy_call)
-    cat_path = cltcache_path / cache_key[0]
-    cache_path = cat_path / cache_key
-    err_path = cache_path.with_suffix(".err.gz")
-    out_path = cache_path.with_suffix(".out.gz")
-    if os.path.exists(cache_path):
-        if os.path.exists(out_path):
-            clang_tidy_stdout = decompress_from_file(out_path)
-            if clang_tidy_stdout:
-                print(clang_tidy_stdout.decode("utf-8"),)
-        if os.path.exists(err_path):
-            clang_tidy_stderr = decompress_from_file(err_path)
-            if clang_tidy_stderr:
-                print(clang_tidy_stderr.decode("utf-8"), file=sys.stderr,)
-        sys.exit(int(read_from_file(cache_path)))
-
+    cltcache_path, config = init_cltcache()
+    cache_path, cat_path, out_path, err_path = (None, None, None, None)
+    verbose = config.getboolean("behavior", "verbose", fallback=False)
+    if verbose:
+        print("cltcache computing cache key")
+    try:
+        cache_key = compute_cache_key(clang_tidy_call, config)
+        if verbose:
+            print("cltcache key:", cache_key)
+        cat_path = cltcache_path / cache_key[0]
+        cache_path = cat_path / cache_key
+        out_path = cache_path.with_suffix(".out.gz")
+        err_path = cache_path.with_suffix(".err.gz")
+        if os.path.exists(cache_path):
+            if verbose:
+                print("cltcache hit!")
+            if os.path.exists(out_path):
+                clang_tidy_stdout = decompress_from_file(out_path)
+                if clang_tidy_stdout:
+                    print(clang_tidy_stdout.decode("utf-8"),)
+            if os.path.exists(err_path):
+                clang_tidy_stderr = decompress_from_file(err_path)
+                if clang_tidy_stderr:
+                    print(clang_tidy_stderr.decode("utf-8"), file=sys.stderr,)
+            sys.exit(int(read_from_file(cache_path)))
+        elif verbose:
+            print("cltcache miss...")
+    except Exception as e:
+        if verbose:
+            print("cltcache", e)
+            print(
+                "cltcache Preprocessing failed! Forwarding call without caching...",
+                file=sys.stderr)
     result = run_command(clang_tidy_call)
-    cat_path.mkdir(parents=True, exist_ok=True)
-
-    save_to_file(str(result.returncode), cache_path)
+    clt_success = result.returncode == 0
+    preproc_success = cache_path is not None
+    cache_results = (clt_success or config.getboolean(
+        "behavior", "cache_failure", fallback=True)) and preproc_success
+    if cache_results:
+        cat_path.mkdir(parents=True, exist_ok=True)
     if result.stdout:
         print(result.stdout.decode("utf-8"),)
-        compress_to_file(result.stdout, out_path)
+        if cache_results:
+            compress_to_file(result.stdout, out_path)
     if result.stderr:
         print(result.stderr.decode("utf-8"), file=sys.stderr,)
-        compress_to_file(result.stderr, err_path)
+        if cache_results:
+            compress_to_file(result.stderr, err_path)
+    if cache_results:
+        if verbose:
+            print("cltcache caching results...")
+        save_to_file(str(result.returncode), cache_path)
     sys.exit(result.returncode)
 
 
